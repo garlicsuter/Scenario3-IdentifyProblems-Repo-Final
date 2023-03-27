cf80abd29c051a63b98d82901fe8b2ad9e3a8c2c
Garlicsuter <markasuter@gmail.com>
2023-03-27 17:48:48 -0400

    Circle stays when truck is clicked

--- Assets/Scripts/SpawnCircle3.cs
+++ Assets/Scripts/SpawnCircle3.cs
@@ -7,58 +7,53 @@ using System.Collections;
 public class SpawnCircle3 : MonoBehaviour
 {
     public GameObject circlePrefab;
-    //public XRController controller;
-    public LayerMask layerMask;
+    public GameObject circleStay;
+    //public LayerMask layerMask;
     public GameObject dragLCtrlHere;
 
     private GameObject circle;
-
-    private InputData _inputData;
-
+    private GameObject newCircleStay;
     private ActionBasedController ABcontrollerL;
     private float buttonPressed;
-
-    public GameObject CubeTest;
-    public bool cubeCastable;
     private bool spawnomatic = true;
 
 
     private void Start()
     {
         ABcontrollerL = dragLCtrlHere.GetComponent<ActionBasedController>();
-        Debug.Log("ABControllerL IS::::::: " + ABcontrollerL);
+        Debug.Log("ABControllerL Is: " + ABcontrollerL);
         //StartCoroutine(Delay1());
-        cubeCastable = CubeTest.GetComponent<XRSimpleInteractable>();
-        Debug.Log("START spawnomatic: " + spawnomatic);
     }
 
     
     private void Update()
     {
         buttonPressed = ABcontrollerL.selectAction.action.ReadValue<float>();
-        Debug.Log("spawnomatic: " + spawnomatic);
-
         RaycastHit hit;
-        
-
 
         if (spawnomatic)
         {
             bool isHit = Physics.Raycast(ABcontrollerL.transform.position, ABcontrollerL.transform.forward, out hit, Mathf.Infinity);
             if (isHit)
             {
+                if (ABcontrollerL.activateAction.action.triggered)
+                {
+                    Debug.Log("trigger pressed");
+                    newCircleStay = Instantiate(circleStay, hit.point, Quaternion.identity);
+                    newCircleStay.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                }
+
                 if (circle == null)
                 {
                     circle = Instantiate(circlePrefab, hit.point, Quaternion.identity);
-                    Debug.Log("Tried to Spawn Circle and isHit:" + isHit);
-
-                    StartCoroutine(Delay1());
+                    Debug.Log("Spawn Circle and isHit: " + isHit);
+                    //StartCoroutine(Delay1());
                 }
                 else
                 {
                     circle.transform.position = hit.point;
                     circle.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
-                    Debug.Log("ELSE ELSE ELSE ELSE & isHit::" + isHit);
+                    Debug.Log("ELSE & isHit: " + isHit);
                 }
             }
             else if (circle != null)
@@ -72,7 +67,6 @@ public class SpawnCircle3 : MonoBehaviour
 
     IEnumerator Delay1()
     {
-        
         yield return 300;
         spawnomatic = false;
         Debug.Log("yielded 300");
@@ -80,7 +74,5 @@ public class SpawnCircle3 : MonoBehaviour
         spawnomatic = true;
         Destroy(circle);
         Debug.Log("yielded 100");
-    }
-    //isHit = false;
-    
+    }    
 }
